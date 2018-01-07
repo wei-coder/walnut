3565d127de9a4624b64d66404a93ff8d082c9c48
wei-coder <34153707+wei-coder@users.noreply.github.com>
2018-01-07 21:14:11 +0800

    Add files via upload

---
+++ init/init.c
@@ -0,0 +1,15 @@
+/*
+filename: init.c
+author:	  wei-coder
+date:	  2018-1
+purpose:  定义了用户态的第一个进程
+*/
+
+#include "init.h"
+#include "console.h"
+
+void init(void)
+{
+	printf("this is process 1!\n");
+}
+
