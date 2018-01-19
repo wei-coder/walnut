ee13835eed5e61b73b1db40f53b9daeb9a6001bc
wei-coder <34153707+wei-coder@users.noreply.github.com>
2018-01-19 18:01:49 +0800

    Add files via upload

--- task/sched.c
+++ task/sched.c
@@ -5,22 +5,23 @@ date:		2018-1
 purpose:	定义了进程调度相关的处理函数
 */
 
+#if 1
 #include "sched.h"
-#include "pm.h"
-#include "system.h"
+#include "console.h"
+
 
 extern int timer_interrupt (void);	// 时钟中断处理程序
 extern int system_call(void);	// 系统调用中断处理程序(kernel/system_call.s,80)。
 
 /*全局描述符表的定义*/
-extern desc_t gdt_entry[GDT_ENTRY_LEN] = {0};
+extern desc_t gdt_entry[GDT_ENTRY_LEN] ;
 
 static union task_union init_task = { INIT_TASK, };							// 定义初始任务的数据
 long volatile jiffies;														// 从开机开始算起的滴答数时间值（10ms/滴答）。
-long startup_time;															// 开机时间。从1970:0:0:0 开始计时的秒数。
-struct task_struct *current = &(init_task.task);							// 当前任务指针（初始化为初始任务）。
+long startup_time;														// 开机时间。从1970:0:0:0 开始计时的秒数。
+struct task_struct *current = &(init_task.task);								// 当前任务指针（初始化为初始任务）。
 struct task_struct *last_task_used_math = NULL;								// 使用过协处理器任务的指针。
-struct task_struct *task[NR_TASKS] = { &(init_task.task), };				// 定义任务指针数组。
+struct task_struct *task[NR_TASKS] = { &(init_task.task), };					// 定义任务指针数组。
 long user_stack[PAGE_SIZE >> 2];											//定义系统堆栈指针，4K。指针指在最后一项。
 stack_t stack_start = {&user_stack[PAGE_SIZE >> 2], _SELECTOR_KER_DS};		//定义任务0的系统堆栈地址
 
@@ -100,24 +101,19 @@ void schedule (void)
 // 调度程序的初始化子程序。
 void sched_init (void)
 {
-	int i;
-	desc_t *p;
-	u8 n = _SELECTOR_TSS;
+	gdt_set_gate(GDT_INDEX_TSS, TSS_FLAG, (u32)(&(init_task.task.tss)), sizeof(tss_struct));
 
-	set_tss_desc((u16*)&gdt_entry[FIRST_TSS_ENTRY], &(init_task.task.tss));
-	//set_ldt_desc (gdt + FIRST_LDT_ENTRY, &(init_task.task.ldt));
-	// 清任务数组和描述符表项（注意i=1 开始，所以初始任务的描述符还在）。
-	p = &gdt_entry[FIRST_TSS_ENTRY];
+	asm volatile("pushfl ; andl $0xffffbfff,(%esp) ; popfl");			// 复位NT 标志。
 
-	_asm pushfd;
-	_asm and dword ptr ss:[esp],0xffffbfff;
-	_asm popfd;
-	_asm{ltr word ptr n}			// 将任务0 的TSS 加载到任务寄存器tr。
+	asm volatile("ltr %%ax"::"a"(GDT_INDEX_TSS*8));				//加载TR寄存器
 
 	//设置时钟中断处理程序句柄（设置时钟中断门）。
-	register_int_handler(IRQ0, &timer_interrupt);
+	//register_int_handler(IRQ0, &timer_interrupt);
 
-  // 设置系统调用中断门。
-	set_system_gate (0x80, &system_call);
+	// 设置系统调用中断门。
+	//set_system_gate (0x80, &system_call);
+	idt_set_gate(0x80, (u32)(&system_call), _SELECTOR_KER_CS, 0x8E);
+	
 }
 
+#endif
