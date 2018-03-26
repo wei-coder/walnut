0b02a3e1a4154179529cef6239202401fe7cafbf
wei-coder <34153707+wei-coder@users.noreply.github.com>
2018-03-26 17:34:06 +0800

    Add files via upload

--- init/init.c
+++ init/init.c
@@ -7,9 +7,13 @@ purpose:  定义了用户态的第一个进程
 
 #include "init.h"
 #include "console.h"
+#include "sched.h"
+
+//extern struct task_struct *current;
+
 
 void init(void)
 {
-	printf("this is process 1!\n");
+	printf("current process is %d!\n", current->pid);
 }
 
